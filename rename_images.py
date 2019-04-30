2b5d9a4d6cc631886548772adfae7eb037938589
cfcv <cfcv@cin.ufpe.br>
2019-04-30 14:18:31 +0200

    final commit BD

--- rename_images.py
+++ rename_images.py
@@ -8,9 +8,12 @@ ap.add_argument('-c', '--count', required=True, help='stating counting')
 args = vars(ap.parse_args())
 
 files = os.listdir(args['source'])
+print("number of files in the directory:",len(files))
 start_count = int(args['count'])
-#print(start_count)
+print(start_count)
 
 for i,f in enumerate(files):
-    new_name = str(i+start_count) + ".jpg"
+    new_name = str(i+start_count) + "." + f[-3:]
     os.rename(args['source'] + "/" + f, args['source'] + "/" + new_name)
+
+print("number of files in the directory after:", len(os.listdir(args['source'])))
