d69fc41d1f59dcf045fb79b9efcf2b705a74c262
cfcv <cfcv@cin.ufpe.br>
2019-04-30 15:38:52 +0200

    pre-traitement: get luminance channel

--- tree_utils.py
+++ tree_utils.py
@@ -1,5 +1,17 @@
 import cv2
 
+#Cchange the colorspace from BGR to HSV
+# input: img in the BGR colorspace
+# output: img in the HSV colorspace
 def to_HSV(img):
     assert(img.shape[2] == 3)
     return cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+# Get only the luminance channel(Y) of the YCrCb space
+# input: img in the BGR colorspace
+# output: Y channel
+def get_Luminance(img):
+    assert(img.shape[2] == 3)
+    img_YCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    Y, Cr, Cb = cv2.split(img_YCrCb)
+    return Y
