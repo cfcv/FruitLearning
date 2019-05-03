654b3b6a3d41224b71065b2e0479a211061ea928
cfcv <cfcv@cin.ufpe.br>
2019-05-03 08:24:25 +0200

    Histogram bug fix

--- tree_utils.py
+++ tree_utils.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+from skimage import feature
 
 #Cchange the colorspace from BGR to HSV
 # input: img in the BGR colorspace
@@ -15,3 +17,33 @@ def get_Luminance(img):
     img_YCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
     Y, Cr, Cb = cv2.split(img_YCrCb)
     return Y
+
+def get_hist(img):
+    #assert(len(img.shape) == 2)
+    n = 4
+    n_bins = n**3
+    
+    r, g, b = cv2.split(img)
+    
+    r = np.floor(r.flatten() / n_bins)
+    g = np.floor(g.flatten() / n_bins)
+    b = np.floor(b.flatten() / n_bins)
+
+    iQ = np.zeros(r.shape)
+    #iQ = np.floor(iQ/n_bins)
+    print("shape:", iQ.shape)
+    iQ = r + n*g + n*n*b
+
+    maximum_channel = int(np.floor(255.0/n_bins))
+    maximum_value = maximum_channel + n*maximum_channel + n*n*maximum_channel
+    pas = (maximum_value+1)/n_bins
+    bins = [i*pas  for i in range(maximum_value+1)]
+    #bins[0] -= 0.001
+    #print(len(bins))
+    #print("bins",bins)
+    #print(np.amax(iQ))
+    return np.histogram(iQ, bins=bins, normalized=True)
+
+def get_LBP(img):
+    assert(len(img.shape) == 2)
+    return feature.local_binary_pattern(img, 8, 2, method='ror')
